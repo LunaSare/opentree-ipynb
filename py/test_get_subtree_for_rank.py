ac87232c7d855217ca2d7ed5d7fa128387bf1df1
Luna Luisa Sanchez Reyes <sanchez.reyes.luna@gmail.com>
2019-12-08 13:36:32 -0800

    Update test_get_subtree_for_rank.py

--- py/test_get_subtree_for_rank.py
+++ py/test_get_subtree_for_rank.py
@@ -1,4 +1,6 @@
 import get_subtree_for_rank
 
+# you cand ownload the taxonomy file from https://tree.opentreeoflife.org/about/taxonomy-version/ott3.1
+# OR download the taxonomy file with this function, it will be saved as ott3.1.tsv in your current dir
 get_subtree_for_rank.download_taxonomy_file()
-get_subtree_for_rank.get_tree(rank = "family", taxonomy_file = '../py/ott3.1.tsv')
+get_subtree_for_rank.get_tree(rank = "family", taxonomy_file = 'replace by path and name of tsv file')
